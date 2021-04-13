4325302b81aaca53e85a9eb742dd87310f73d169
fmolinie <fmolinie@sqli.com>
2021-04-13 12:15:46 +0200

    3B - logout - 15min

--- colorGameTest/src/app/user/logout/logout.component.ts
+++ colorGameTest/src/app/user/logout/logout.component.ts
@@ -1,15 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { Router } from "@angular/router";
+import { UserService } from "../../user.service";
 
 @Component({
   selector: 'app-logout',
   templateUrl: './logout.component.html',
   styleUrls: ['./logout.component.scss']
 })
-export class LogoutComponent implements OnInit {
+export class LogoutComponent implements OnDestroy {
+  subscription: Subscription | undefined;
 
-  constructor() { }
+  constructor(private readonly router: Router,
+              private readonly userService: UserService) { }
 
-  ngOnInit(): void {
+  ngOnDestroy() {
+    if (this.subscription) {
+      this.subscription.unsubscribe();
+    }
+  }
+
+  /**
+   * on logout
+   */
+  logout(): void {
+    this.subscription = this.userService.logout().subscribe(() => {
+      this.router.navigate(['/home']).then(nav => {
+        console.log(nav); // true if navigation is successful
+      }, err => {
+        console.log(err) // when there's an error
+      });
+    });
   }
 
 }
